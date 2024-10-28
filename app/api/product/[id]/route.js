2c718aa1ae347108fca999458ea37ffd09b15774
Tufan Korkmazel <tufankorkmazel3@gmail.com>
2024-10-29 02:39:10 +0300

    güncellendi

--- app/api/product/[id]/route.js
+++ app/api/product/[id]/route.js
@@ -1,7 +1,7 @@
 //  app\api\product\[id]\route.js
 import { supabase } from '@/supabase/supabaseClient';
 
-// Ürünü ID ile güncelleme
+//  ID ile güncelleme
 export async function PATCH(req, { params }) {
     const { id } = params;
     const { name, quantity, price } = await req.json();
@@ -14,7 +14,7 @@ export async function PATCH(req, { params }) {
     return new Response(JSON.stringify(data), { status: 200 });
 }
 
-// Ürünü ID ile silme
+//  ID ile silme
 export async function DELETE(req, { params }) {
     const { id } = params;
     const { data, error } = await supabase.from('products').delete().eq('id', id);
